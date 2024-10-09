4dfc27b8c8da523ea8e9fc2af79d91c5543e78ac
Mirnafebriasari <mirnhafebriasari@gmail.com>
2024-10-09 16:39:21 +0700

    Update Brightness.py

--- Editing_gambar/Brightness.py
+++ Editing_gambar/Brightness.py
@@ -1,45 +1,45 @@
 from PIL import Image, ImageEnhance
+from IPython.display import display
 import os
 
-def adjust_brightness(image_path, output_path, level):
+def adjust_brightness(image_path, output_path, level, show_image=True):
     """Mengubah kecerahan gambar berdasarkan level yang ditentukan.
+    
     Args:
         image_path (str): Jalur gambar dari folder.
         output_path (str): Jalur gambar yang akan disimpan.
-        level (float): Tingkat kecerahan yang diinginkan (angka antara 0.0 dan 2.0)."""
-
+        level (float): Tingkat kecerahan yang diinginkan (angka antara 0.0 dan 2.0).
+        show_image (bool): Menentukan apakah gambar hasil akan ditampilkan. Default adalah True.
+    """
+    
     # Cek apakah level adalah angka (float)
     if not isinstance(level, (int, float)):
         print("Level kecerahan harus berupa angka.")
         return
 
-    # Mengkonversi level menjadi float
     factor = float(level)
 
-    # Menambahkan peringatan jika level tidak valid
     if factor < 0.0 or factor > 2.0:
-        print("Level kecerahan tidak valid. Harus antara 0.0 dan 2.0. Menggunakan kecerahan 'normal' (1.0).")
-        factor = 1.0  # Default ke 'normal' jika level tidak valid
+        print("Level kecerahan tidak valid. Menggunakan kecerahan 'normal' (1.0).")
+        factor = 1.0
 
-    # Cek apakah file gambar ada
     if not os.path.isfile(image_path):
         print(f"File gambar tidak ditemukan: {image_path}")
         return
 
-    image = Image.open(image_path)  # Membuka file gambar
-    enhancer = ImageEnhance.Brightness(image)  # Menggunakan fungsi pencerahan dari Pillow
-    brightened_image = enhancer.enhance(factor)  # Menentukan skala kecerahan gambar
+    image = Image.open(image_path)
+    enhancer = ImageEnhance.Brightness(image)
+    brightened_image = enhancer.enhance(factor)
 
-    # Memeriksa apakah jalur output sudah ada
-    if os.path.isfile(output_path):
-        overwrite = input(f"File sudah ada di {output_path}. Apakah Anda ingin menimpa? (y/n): ")
-        if overwrite.lower() != 'y':
-            print("Proses dibatalkan.")
-            return
+    brightened_image.save(output_path)
+    print(f"File disimpan di: {output_path}")
 
-    brightened_image.save(output_path)  # Menyimpan hasil di jalur output
-    brightened_image.show()  # Menunjukkan hasil pencerahan gambar
+    # Tampilkan gambar jika show_image adalah True
+    if show_image:
+        display(brightened_image)
 
 # Contoh penggunaan
-# adjust_brightness("C:\\Users\\Asus\\Documents\\y.jpg", "C:\\Users\\Asus\\Documents\\dkk.jpg", 0.3)
+#adjust_brightness("/content/y.jpg", "/content/Brg.jpg", 0.4, show_image=True)
 
+# Cek file yang ada
+#!ls /content/
