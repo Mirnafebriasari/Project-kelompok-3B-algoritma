4f6f10772471f7db5c05d1b287c5610ec5bddd4b
Mirnafebriasari <mirnhafebriasari@gmail.com>
2024-10-09 13:37:26 +0700

    Update __init__.py comment help

--- Editing_gambar/__init__.py
+++ Editing_gambar/__init__.py
@@ -1,4 +1,4 @@
-from .convert_to_grayscale import convert_image_to_grayscale
+from .convert_to_grayscale import convert_to_grayscale
 from .rotate_image import rotate_image
 from .Brightness import adjust_brightness
 from .penambahanTeks import tambahkan_teks
@@ -6,7 +6,7 @@ from .flipping import flip_image
 from .pemotongan_gambar import ukuran_gambar
 
 __all__ = [
-    'convert_image_to_grayscale',
+    'convert_to_grayscale',
     'rotate_image',
     'adjust_brightness',
     'tambahkan_teks',
